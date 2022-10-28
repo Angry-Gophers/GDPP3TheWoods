bcb71127d24d22de910978df9641aa541f6bb2d5
TDHall2 <tdhall2@student.fullsail.edu>
2022-10-28 14:07:19 -0400

    Player can jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float playerSpeed;
     [SerializeField] float jumpHeight;
     [SerializeField] int jumpMax;
+    [SerializeField] float gravityValue;
     Vector3 playervelocity;
     int timesJumped;
     
@@ -22,12 +23,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement(); 
+        movement();
+        jump();
     }
     
     void movement()
     {
+        if (controller.isGrounded && playervelocity.y < 0)
+        {
+            timesJumped = 0;
+            playervelocity.y = 0;
+        }
+
         Vector3 move = transform.right * (Input.GetAxis("Horizontal")) + transform.forward * (Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
+    }
+
+    void jump()
+    {
+
+        if(Input.GetButton("Jump")&& timesJumped < jumpMax)
+        {
+            timesJumped++;
+            playervelocity.y = jumpHeight;
+        }
+        playervelocity.y -= gravityValue * Time.deltaTime;
+        controller.Move(playervelocity * Time.deltaTime);
+
+
     }
 }
