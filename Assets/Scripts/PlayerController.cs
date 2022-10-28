d55c048fb6be9f8df7e8a5493ed377767eda7bd4
TDHall2 <tdhall2@student.fullsail.edu>
2022-10-28 15:20:27 -0400

    Added player HP and Original HP, added Tag Player to Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IDamage
 {
     [Header("---Components---")]
     [SerializeField] CharacterController controller;
     [Header("---Player Stats---")]
+    [SerializeField] int HP;
     [SerializeField] float playerSpeed;
     [SerializeField] float jumpHeight;
     [SerializeField] int jumpMax;
     [SerializeField] float gravityValue;
+    [SerializeField] int hpOriginal;
     [Header("---Player Weapon Stats---")]
     [SerializeField] int shootDmg;
     [SerializeField] float shootDist;
@@ -23,6 +25,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        HP = hpOriginal;
         
     }
 
@@ -78,5 +81,15 @@ public class PlayerController : MonoBehaviour
         }
         
         
+    }
+
+    public void takeDamage(int dmg)
+    {
+        HP -= dmg;
+        if(HP <= 0)
+        {
+            Debug.Log("You Died");
+        }
+
     }
 }
