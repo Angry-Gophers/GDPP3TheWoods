7351f51d178a02c547216af31ed2753c903700fc
Kyle Coger <kylecoger17@gmail.com>
2022-10-28 20:12:50 -0400

    Updating PlayerController for weapons

--- Assets/Scripts/IDamage.cs
+++ Assets/Scripts/IDamage.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public interface IDamage
 {
-    public void takeDamage(int dmg);
+    void takeDamage(int dmg);
 }
