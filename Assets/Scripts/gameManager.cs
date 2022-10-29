ea2e17272efc7338f8ebed02465e3a962f402904
Kyle Coger <kylecoger17@gmail.com>
2022-10-28 21:51:23 -0400

    Cursor for gun/knife, notes in gamemanager

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 public class gameManager : MonoBehaviour
 {
     public static gameManager instance;
+    [Header("----- Player -----")]
     public GameObject player;
     public PlayerController playerScript;
+    // public GameObject spawnPosition;
 
+    //  [Header("----- UI -----")]
+    //  public GameObject pauseMenu;
+    //  public GameObject playerDeadMenu;
+    //  public GameObject shopWindow;
+    //  public GameObject menuCurrentlyOpen;
+    //  public GameObject playerDamageFlash;
+    //  public Image playerHPBar;
+    //  public Image fire;
+    //  public Image ammo;
+    //  public Image traps;
+    //  public Image boards;
+    //  public Image bandages;
+    //  public TextMeshProUGUI fireHealthText;
+    //  public TextMeshProUGUI ammoTracker;
+    //  public TextMeshProUGUI boardsTracker;
+    //  public TextMeshProUGUI trapsTracker;
+    //  public TextMeshProUGUI bandageTracker;
+
+    public bool isPaused;
     void Start()
     {
         instance = this;
@@ -18,6 +39,37 @@ public class gameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Cancel")) // check for deadMenu and shopMenu
+        {
+            isPaused = !isPaused;
+            //pauseMenu.SetActive(isPaused);
+
+            if (isPaused)
+            {
+                cursorLockPause();
+            }
+            else
+            {
+                cursorUnlockUnpause();
+            }
+        }
+    }
+
+    public void cursorLockPause()
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+    public void cursorUnlockUnpause()
+    {
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void updatePlayerHUD()
+    {
+        // ammo, bandages, boards, traps, fire health, anything else? night time left? 
     }
 }
